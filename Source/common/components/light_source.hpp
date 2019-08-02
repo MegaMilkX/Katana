dcdf0cad0b0ce1db0661de059a3f228e684435fc
megamilkx <gyrocopter3k@gmail.com>
2019-08-03 01:02:39 +0300

    Make OmniLight properties linkable

--- Source/common/components/light_source.hpp
+++ Source/common/components/light_source.hpp
@@ -2,33 +2,23 @@
 #define LIGHT_SOURCE_HPP
 
 #include "component.hpp"
+#include "attrib_instantiable.hpp"
 #include "../../common/util/log.hpp"
 
-class OmniLight : public Attribute {
+class OmniLight : public AttribInstantiable<OmniLight> {
     RTTR_ENABLE(Attribute)
 public:
-    gfxm::vec3 color = gfxm::vec3(1,1,1);
-    float intensity = 1.0f;
-    float radius = 5.0f;
+    ktProperty<gfxm::vec3> color = gfxm::vec3(1,1,1);
+    ktProperty<float> intensity = 5.0f;
+    ktProperty<float> radius = 5.0f;
 
     ~OmniLight();
     virtual void onCreate();
 
-    virtual void copy(Attribute* other) {
-        if(other->get_type() != get_type()) {
-            LOG("Can't copy from " << other->get_type().get_name().to_string() << " to " <<
-                get_type().get_name().to_string());
-            return;
-        }
-        OmniLight* o = (OmniLight*)other;
-        color = o->color;
-        intensity = o->intensity;
-        radius = o->radius;
-    }
     virtual bool serialize(out_stream& out) {
-        out.write(color);
-        out.write(intensity);
-        out.write(radius);
+        out.write(color.get());
+        out.write(intensity.get());
+        out.write(radius.get());
         return true;
     }
     virtual bool deserialize(in_stream& in, size_t sz) {
@@ -38,9 +28,9 @@ public:
         return true;
     }
     void onGui() {
-        ImGui::ColorEdit3("color", (float*)&color);
-        ImGui::DragFloat("intensity", &intensity, 0.001f);
-        ImGui::DragFloat("radius", &radius, 0.001f);
+        ImGui::ColorEdit3("color", (float*)&color.get());
+        ImGui::DragFloat("intensity", &intensity.get(), 0.01f);
+        ImGui::DragFloat("radius", &radius.get(), 0.01f);
     }
     void onGizmo(GuiViewport& vp);
     virtual const char* getIconCode() const { return ICON_MDI_LIGHTBULB_ON; }
