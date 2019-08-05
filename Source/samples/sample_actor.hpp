2d229e75113a4a39bed73d3dc634c1afd9095887
megamilkx <gyrocopter3k@gmail.com>
2019-08-05 03:12:21 +0300

    Add empty actor sample

---
+++ Source/samples/sample_actor.hpp
@@ -0,0 +1,22 @@
+#ifndef SAMPLE_ACTOR_HPP
+#define SAMPLE_ACTOR_HPP
+
+#include <common/components/actors/actor.hpp>
+#include <common/world.hpp>
+
+class actorSample : public ktActor {
+    RTTR_ENABLE(ktActor)
+public:
+    void onStart(ktWorld* w) override {
+        // TODO:
+    }
+    void onUpdate() override {
+        // TODO:
+    }
+    void onCleanup() {
+        // TODO:
+    }
+};
+REG_ATTRIB(actorSample, SampleActor, Samples)
+
+#endif
