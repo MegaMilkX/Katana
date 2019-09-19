bbc3d53370f3bda80f7d42b3be482b9f1b646f14
megamilkx <gyrocopter3k@gmail.com>
2019-09-20 02:29:18 +0300

    Add octree expansion

--- Source/common/util/octree.cpp
+++ Source/common/util/octree.cpp
@@ -1,7 +1,7 @@
 #include "octree.hpp"
 
-size_t Octree::acquireCell() {
-    size_t cid = 0;
+int32_t Octree::acquireCell() {
+    int32_t cid = 0;
     if(free_cells.empty()) {
         cid = cells.size();
         cells.emplace_back(OctreeCell());
@@ -11,6 +11,68 @@ size_t Octree::acquireCell() {
     }
     return cid;
 }
-void Octree::freeCell(size_t id) {
+void Octree::freeCell(int32_t id) {
     free_cells.insert(id);
+}
+
+void Octree::expand(const gfxm::aabb& aabb) {
+    OctreeCell& root = cells[root_id];
+    const gfxm::vec3 root_center = (root.aabb.from + root.aabb.to) * .5f;
+    const gfxm::vec3 target_center = (aabb.from + aabb.to) * .5f;
+
+    int8_t root_child_id = 0;
+    if(root_center.x > target_center.x) root_child_id |= 1;
+    if(root_center.y > target_center.y) root_child_id |= 2;
+    if(root_center.z > target_center.z) root_child_id |= 4;
+
+    int32_t new_root_id = acquireCell();
+    root = cells[root_id]; // root reference gets invalidated after acquireCell()
+    OctreeCell& new_root = cells[new_root_id];
+    new_root.children[root_child_id] = root_id;
+
+    const gfxm::vec3 extents(root.aabb.to.x - root.aabb.from.x, root.aabb.to.y - root.aabb.from.y, root.aabb.to.z - root.aabb.from.z);
+    const gfxm::vec3 id_mods(
+      (root_child_id & 1) - .5f,
+      ((root_child_id & 2) >> 1) - .5f,
+      ((root_child_id & 4) >> 2) - .5f
+    );
+
+    new_root.aabb = gfxm::aabb(
+        gfxm::vec3(
+            root.aabb.from.x - (id_mods.x + .5f) * extents.x,
+            root.aabb.from.y - (id_mods.y + .5f) * extents.y,
+            root.aabb.from.z - (id_mods.z + .5f) * extents.z
+        ),
+        gfxm::vec3(
+            root.aabb.to.x - (id_mods.x - .5f) * extents.x,
+            root.aabb.to.y - (id_mods.y - .5f) * extents.y,
+            root.aabb.to.z - (id_mods.z - .5f) * extents.z
+        )  
+    );
+    root_id = new_root_id;    
+}
+
+
+Octree::Octree()
+: root_id(0) {
+    cells.resize(1);
+    cells[0].aabb = gfxm::aabb(
+        gfxm::vec3(-10, -10, -10),
+        gfxm::vec3(10, 10, 10)
+    );
+}
+
+int32_t Octree::fit(const gfxm::aabb& aabb) {
+    while(!gfxm::aabb_in_aabb(aabb, cells[root_id].aabb)) {
+        expand(aabb);
+    }
+
+    return 0;
+}
+
+
+void Octree::debugDraw(DebugDraw& dd) {
+    for(auto& cell : cells) {
+        dd.aabb(cell.aabb, gfxm::vec3(1.0f, 0.7f, 0.0f));
+    }
 }
--- Source/common/util/octree.hpp
+++ Source/common/util/octree.hpp
@@ -6,18 +6,39 @@
 #include <vector>
 #include <set>
 
+#include "../debug_draw.hpp"
+
+enum OCTREE_CELL_CHILD_ID {
+    OCTREE_CELL_FRONT_LEFT_BOTTOM, // 000
+    OCTREE_CELL_FRONT_RIGHT_BOTTOM, // 001
+    OCTREE_CELL_FRONT_RIGHT_TOP, // 010
+    OCTREE_CELL_FRONT_LEFT_TOP, // 011
+    OCTREE_CELL_BACK_LEFT_BOTTOM, // 100
+    OCTREE_CELL_BACK_RIGHT_BOTTOM, // 101
+    OCTREE_CELL_BACK_RIGHT_TOP, // 110
+    OCTREE_CELL_BACK_LEFT_TOP // 111
+};
+
 struct OctreeCell {
     gfxm::aabb aabb;
+    int32_t children[8];
 };
 
 class Octree {
     std::vector<OctreeCell> cells;
-    std::set<size_t> free_cells;
+    std::set<int32_t> free_cells;
+    int32_t root_id;
 
-    size_t acquireCell();
-    void freeCell(size_t id);
+    int32_t acquireCell();
+    void    freeCell(int32_t id);
+
+    void    expand(const gfxm::aabb& aabb);
 
 public:
+    Octree();
+    int32_t fit(const gfxm::aabb& aabb);
+
+    void    debugDraw(DebugDraw& dd);
 
 };
 
