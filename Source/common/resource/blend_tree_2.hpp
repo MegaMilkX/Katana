c0ad0e3ba944382e48de310b767f5f015cc1d9d9
megamilkx <gyrocopter3k@gmail.com>
2019-12-20 11:36:07 +0300

    ~

--- Source/common/resource/blend_tree_2.hpp
+++ Source/common/resource/blend_tree_2.hpp
@@ -3,9 +3,22 @@
 
 #include "resource.h"
 
+#include "../gfxm.hpp"
+
+struct BlendTree2Node {
+    std::string name;
+    gfxm::vec3 gui_color;
+    gfxm::vec2 gui_pos;
+    std::vector<size_t> input_ids;
+    std::vector<size_t> output_ids;
+};
+
 class BlendTree2 : public Resource {
     RTTR_ENABLE(Resource)
 public:
+    std::vector<int>            data_points;
+    std::vector<BlendTree2Node> nodes;
+
 };
 STATIC_RUN(BlendTree2) {
     rttr::registration::class_<BlendTree2>("BlendTree2")
--- Source/editor/doc_blend_tree_2.cpp
+++ Source/editor/doc_blend_tree_2.cpp
@@ -2,15 +2,118 @@
 
 #include "../common/util/imgui_ext.hpp"
 
+struct BlendTree2NodePointDesc {
+    std::string name;
+    rttr::type  type;
+};
+
+struct BlendTree2NodeDesc {
+    std::string name;
+    gfxm::vec3 gui_color;
+    std::vector<BlendTree2NodePointDesc> inputs;
+    std::vector<BlendTree2NodePointDesc> outputs;
+};
+
+static std::map<std::string, BlendTree2NodeDesc>& getBT2NodeDescMap() {
+    static std::map<std::string, BlendTree2NodeDesc> map;
+    return map;
+}
+
+class regBlendTreeNode {
+    BlendTree2NodeDesc* desc_ptr;
+public:
+    regBlendTreeNode(const char* name) {
+        desc_ptr = &getBT2NodeDescMap()[name];
+        desc_ptr->name = name;
+    }
+    regBlendTreeNode& gui_color(float r, float g, float b) {
+        desc_ptr->gui_color = gfxm::vec3(r, g, b);
+        return *this;
+    }
+    template<typename T>
+    regBlendTreeNode& in(const char* name) {
+        desc_ptr->inputs.emplace_back({ name, rttr::type::get<T>() });
+        return *this;
+    }
+    template<typename T>
+    regBlendTreeNode& out(const char* name) {
+        desc_ptr->outputs.emplace_back({ name, rttr::type::get<T>() });
+        return *this;
+    }
+    regBlendTreeNode& job(void(*_job)(void)) {
+        // TODO:
+        return *this;
+    }
+};
+
+static int foo() {
+    regBlendTreeNode("AnimSample")
+        .gui_color(0.6f, 0.0f, 0.2f)
+        .out<int>("pose")
+        .job([](){
+
+        });
+    regBlendTreeNode("Blend2")
+        .gui_color(0.6f, 0.0f, 0.2f)
+        .in<int>("a")
+        .in<int>("b")
+        .in<float>("weight")
+        .out<int>("result")
+        .job([](){
+
+        });
+    regBlendTreeNode("Blend3")
+        .gui_color(0.6f, 0.0f, 0.2f)
+        .in<int>("a")
+        .in<int>("b")
+        .in<int>("c")
+        .in<float>("weight")
+        .out<int>("result")
+        .job([](){
+
+        });
+    regBlendTreeNode("Parameter")
+        .gui_color(0.6f, 0.0f, 0.2f)
+        .out<float>("value")
+        .job([](){
+
+        });
+    return 0;
+}
+
 void DocBlendTree2::onGui(Editor* ed, float dt) {
+    BlendTree2 bt;
+    bt.nodes.emplace_back(BlendTree2Node{
+        "Blend3", 
+        gfxm::vec3(0.6f, 0.0f, 0.3f),
+        gfxm::vec2(0,0),
+        { 1, 2 },
+        { 3 }
+    });
+    bt.nodes.emplace_back(BlendTree2Node{
+        "AnimSample",
+        gfxm::vec3(0.0f, 0.6f, 0.3f),
+        gfxm::vec2(-200,-40),
+        { },
+        { 4 }
+      });
+
     if(ImGuiExt::BeginGridView("BlendTree2Grid")) {
-        ImVec2 pos(0,0);
-        ImGuiExt::BeginTreeNode("Test", &pos, 0, false, ImVec2(100, 100), ImGui::ColorConvertFloat4ToU32(ImVec4(.5, 0.2, 0.0, 1.0f)));
+        
+        for(auto& n : bt.nodes) {
+            ImVec2 pos(n.gui_pos.x, n.gui_pos.y);
+            ImGuiExt::BeginTreeNode(n.name.c_str(), &pos, 0, false, ImVec2(100, 100),
+                ImGui::ColorConvertFloat4ToU32(ImVec4(n.gui_color.x, n.gui_color.y, n.gui_color.z, 1.0f)));
 
-        ImGuiExt::TreeNodeIn("test", 0, 0);
-        ImGuiExt::TreeNodeOut("result", 0, 0);
+            for(auto& in : n.input_ids) {
+                ImGuiExt::TreeNodeIn("test", in, 0);
+            }
+            for(auto& out : n.output_ids) {
+                ImGuiExt::TreeNodeOut("test", out, 0);
+            }
 
-        ImGuiExt::EndTreeNode();
+            ImGuiExt::EndTreeNode();
+        }
     }
     ImGuiExt::EndGridView();
 }
