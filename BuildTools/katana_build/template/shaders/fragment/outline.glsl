4a9bd37b60ff5f72ce88389333db42ad29328446
megamilkx <gyrocopter3k@gmail.com>
2020-05-05 00:27:34 +0300

    Update shaders

--- BuildTools/katana_build/template/shaders/fragment/outline.glsl
+++ BuildTools/katana_build/template/shaders/fragment/outline.glsl
@@ -6,36 +6,15 @@ uniform sampler2D tex_0;
 
 out vec4 out_albedo;
 
-vec4 blur5(sampler2D image, vec2 uv, vec2 resolution, vec2 direction) {
-    vec4 color = vec4(0.0);
-    vec2 off1 = vec2(1.3333333333333333) * direction;
-    color += texture2D(image, uv) * 0.29411764705882354;
-    color += texture2D(image, uv + (off1 / resolution)) * 0.35294117647058826;
-    color += texture2D(image, uv - (off1 / resolution)) * 0.35294117647058826;
-    return color; 
-}
-vec4 blur13(sampler2D image, vec2 uv, vec2 resolution, vec2 direction) {
-    vec4 color = vec4(0.0);
-    vec2 off1 = vec2(1.411764705882353) * direction;
-    vec2 off2 = vec2(3.2941176470588234) * direction;
-    vec2 off3 = vec2(5.176470588235294) * direction;
-    color += texture2D(image, uv) * 0.1964825501511404;
-    color += texture2D(image, uv + (off1 / resolution)) * 0.2969069646728344;
-    color += texture2D(image, uv - (off1 / resolution)) * 0.2969069646728344;
-    color += texture2D(image, uv + (off2 / resolution)) * 0.09447039785044732;
-    color += texture2D(image, uv - (off2 / resolution)) * 0.09447039785044732;
-    color += texture2D(image, uv + (off3 / resolution)) * 0.010381362401148057;
-    color += texture2D(image, uv - (off3 / resolution)) * 0.010381362401148057;
-    return color;
-}
+const float offset = 1.0 / 512.0;
+
 
 void main()
 {
-    vec4 blurred = blur13(tex_0, uv_frag, vec2(256, 256), vec2(1, 0));
-    blurred += blur13(tex_0, uv_frag, vec2(256, 256), vec2(0, 1));
-    blurred = clamp(blurred, vec4(0.0), vec4(1.0));
-    vec4 border = (vec4(blurred.xxxx) - vec4(texture(tex_0, uv_frag).xxxx));
-    border *= 2.0f;
-    border = clamp(border, vec4(0.0), vec4(1.0));
-    out_albedo = border;
+    vec4 col = texture2D(tex_0, uv_frag);
+	float a = texture(tex_0, vec2(uv_frag.x + offset, uv_frag.y)).r
+		+ texture2D(tex_0, vec2(uv_frag.x, uv_frag.y - offset)).r
+		+ texture2D(tex_0, vec2(uv_frag.x - offset, uv_frag.y)).r
+		+ texture2D(tex_0, vec2(uv_frag.x, uv_frag.y + offset)).r;
+	out_albedo = vec4(a,a,a,a);
 }
